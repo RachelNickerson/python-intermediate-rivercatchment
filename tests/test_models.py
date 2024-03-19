8c59391d55ca4a4926f95837cb3f5d266177efc7
Rachel Nickerson <racnic@ceh.ac.uk>
2024-03-19 10:51:27 +0000

    Update to test models

--- tests/test_models.py
+++ tests/test_models.py
@@ -4,7 +4,6 @@ import pandas as pd
 import pandas.testing as pdt
 import datetime
 
-
 def test_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""
     from catchment.models import daily_mean
@@ -33,9 +32,9 @@ def test_daily_mean_integers():
     from catchment.models import daily_mean
 
     test_input = pd.DataFrame(
-                     data=[[1, 2],
-                           [3, 4],
-                           [5, 6]],
+                     data=[[1.0, 2.0],
+                           [3.0, 4.0],
+                           [5.0, 6.0]],
                      index=[pd.to_datetime('2000-01-01 01:00'),
                             pd.to_datetime('2000-01-01 02:00'),
                             pd.to_datetime('2000-01-01 03:00')],
@@ -49,3 +48,55 @@ def test_daily_mean_integers():
 
     # Need to use Pandas testing functions to compare arrays
     pdt.assert_frame_equal(daily_mean(test_input), test_result)
+
+def test_daily_max():
+    """Test that max function works for an array of positive integers."""
+    from catchment.models import daily_max
+
+    test_input = pd.DataFrame(
+                      data=[[4, 2, 5],
+                            [1, 6, 2],
+                            [4, 1, 9]],
+                      index=[pd.to_datetime('2000-01-01 01:00'),
+                             pd.to_datetime('2000-01-01 02:00'),
+                             pd.to_datetime('2000-01-01 03:00')],
+                      columns=['A', 'B', 'C']
+    )
+    test_result = pd.DataFrame(
+                      data=[[4, 6, 9]],
+                      index=[datetime.date(2000, 1, 1)],
+                      columns=['A', 'B', 'C']
+    )
+
+    pdt.assert_frame_equal(daily_max(test_input), test_result)
+
+
+def test_daily_min():
+    """Test that min function works for an array of positive and negative integers."""
+    from catchment.models import daily_min
+
+    test_input = pd.DataFrame(
+                      data=[[ 4, -2, 5],
+                            [ 1, -6, 2],
+                            [-4, -1, 9]],
+                      index=[pd.to_datetime('2000-01-01 01:00'),
+                             pd.to_datetime('2000-01-01 02:00'),
+                             pd.to_datetime('2000-01-01 03:00')],
+                      columns=['A', 'B', 'C']
+    )
+    test_result = pd.DataFrame(
+                      data=[[-4, -6, 2]],
+                      index=[datetime.date(2000, 1, 1)],
+                      columns=['A', 'B', 'C']
+    )
+
+    pdt.assert_frame_equal(daily_min(test_input), test_result)
+
+import pytest
+...
+def test_daily_min_python_list():
+    """Test for AttributeError when passing a python list"""
+    from catchment.models import daily_min
+
+    with pytest.raises(AttributeError):
+        error_expected = daily_min([[3, 4, 7],[-3, 0, 5]])
